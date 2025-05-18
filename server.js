6e725a8eca1e6948a9a42f4ce9e33a8aacc78d94
Om Thakur <82919861+MonsterFlick@users.noreply.github.com>
2025-05-18 16:36:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.all('/api/webhook', (req, res) => handler(req, res));
 
-const port = 3021;
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
